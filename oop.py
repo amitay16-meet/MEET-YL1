cb696a819a31831c4e27ad142adb84924f88a041
Guest <guest-NMnNHB@meet.mit.edu>
2015-01-15 20:05:49 +0200

    opp.py

---
+++ oop.py
@@ -0,0 +1,11 @@
+class animal:
+	def _init_(self,name,age,color,size):
+		self.name=name 
+		self.age=age
+		self.color=color	
+		self.size=size
+	
+dog=animal("sadek",22,"yellow","tiny")
+print(dog.name)
+
+
